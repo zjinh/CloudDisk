428cb435085e957bffe0e245098d0f55c6cafdec
ZJINH <2665229856@qq.com>
2019-07-04 10:48:12 +0800

    修复登录401的问题，，

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import axios from 'axios'
 import Api from "./api/api"
 import App from './App'
 import router from './router'
@@ -28,31 +27,6 @@ Vue.component('Icon',Icon);
 Vue.component('Spin',Spin);
 Vue.component('Progress',Progress);
 Vue.prototype.$IVIEW = {};
-axios.interceptors.response.use(function (response) {
-// 对响应数据做点什么
-    return response;
-}, function (err) {
-    if (err && err.response) {
-        switch (err.response.status) {
-            case 400: err.message = '请求错误(400)' ; break;
-            case 401: err.message = '未授权，请重新登录(401)'; break;
-            case 403: err.message = '拒绝访问(403)'; break;
-            case 404: err.message = '请求出错(404)'; break;
-            case 408: err.message = '请求超时(408)'; break;
-            case 500: err.message = '服务器错误(500)'; break;
-            case 501: err.message = '服务未实现(501)'; break;
-            case 502: err.message = '网络错误(502)'; break;
-            case 503: err.message = '服务不可用(503)'; break;
-            case 504: err.message = '网络超时(504)'; break;
-            case 505: err.message = 'HTTP版本不受支持(505)'; break;
-            default: err.message = `连接出错(${err.response.status})!`;
-        }
-    }else{
-        err.message = '连接服务器失败，请检查网络连接或联系管理员'
-    }
-    Message.error(err.message);
-    return Promise.reject(err);
-});
 //引入element的部分组件
 import { MessageBox,Dialog} from 'element-ui';
 Vue.prototype.$msgbox = MessageBox;
@@ -88,8 +62,6 @@ Vue.path = Vue.prototype.$path = path;//path接口
 Vue.ipc = Vue.prototype.$ipc = ipc;//ipc接口
 Vue.api = Vue.prototype.$Api = Api;//请求接口
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
-axios.defaults.withCredentials=true;
-Vue.http = Vue.prototype.$http = axios;
 Vue.Notify = Vue.prototype.$Notify =(msg)=>{
     new Notification('CloudDisk',{
         body: msg
